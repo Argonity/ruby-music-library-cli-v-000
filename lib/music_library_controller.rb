f560b5107c25bab49479e2df6656e996828a0097
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-12 18:06:12 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,8 +29,7 @@ class MusicLibraryController
     def list_songs
       Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-      print
-    end
+      end
     end
 
     def list_artists
