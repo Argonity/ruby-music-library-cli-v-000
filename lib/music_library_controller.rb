e22be8f5594dafd090e2e53fdc9204a10c6eda01
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-12 18:10:00 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,12 +36,8 @@ class MusicLibraryController
       when "play song"
         play_song
       end
-
     end
-
   end
-
-
 
     def list_songs
       Song.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
